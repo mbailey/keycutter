b32a1d088531c71ceac2660c292fa71a21bec3ac
Mike Bailey <mike@bailey.net.au>
2025-02-06 18:29:03 +1100

    refactor: Simplify keycutter script and remove unused commands

--- shell/completions/keycutter.bash
+++ shell/completions/keycutter.bash
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 _keycutter_completion() {
-    local cur prev words cword
-    _init_completion || return
+  local cur prev words cword
+  _init_completion || return
 
-    local commands="create authorized-keys list update update-git update-ssh-config"
+  local commands="create authorized-keys update"
 
-    case $prev in
-        keycutter)
-            COMPREPLY=($(compgen -W "$commands" -- "$cur"))
-            return
-            ;;
-        create)
-            # Add completion for create subcommand options
-            COMPREPLY=($(compgen -W "--resident --type" -- "$cur"))
-            return
-            ;;
-        --type)
-            COMPREPLY=($(compgen -W "ecdsa-sk ed25519-sk rsa ecdsa ed25519" -- "$cur"))
-            return
-            ;;
-    esac
+  case $prev in
+  keycutter)
+    COMPREPLY=($(compgen -W "$commands" -- "$cur"))
+    return
+    ;;
+  create)
+    # Add completion for create subcommand options
+    COMPREPLY=($(compgen -W "--resident --type" -- "$cur"))
+    return
+    ;;
+  --type)
+    COMPREPLY=($(compgen -W "ecdsa-sk ed25519-sk rsa ecdsa ed25519" -- "$cur"))
+    return
+    ;;
+  esac
 
-    # Default to filename completion
-    COMPREPLY=($(compgen -f -- "$cur"))
+  # Default to filename completion
+  COMPREPLY=($(compgen -f -- "$cur"))
 }
 
 complete -F _keycutter_completion keycutter
